43ff25c632d88d5707aa8deb567844461c1eed0a
Udbhav-Dubey <udbhavdubey44@gmail.com>
2026-06-07 14:10:05 +0530

    small prediction module added to atleast get final answer , now 2 biggest module are  remaning 1) image to tensor  , 2) real weights converter to our file format

---
+++ src/prediction.cpp
@@ -0,0 +1,17 @@
+// final input must be size 10 this is in architecure of the engine , if something changes please notify and change the code 
+// currently if same prediction that is not dealt with currently return the first one 
+#include "prediction.h"
+int prediction(Tensor&input){
+    int size=input.gsize();
+    assert(size==10&&"the final tensor must be size of 10 to get output\n");
+    int ans{0};
+    float*in=input.data();
+    float maxdata{in[0]};
+    for (int i=1;i<10;i++){
+        if (in[i]>maxdata){
+           ans=i;
+           maxdata=in[i];
+        }
+    }
+    return ans;
+}
--- test/loadertest2.cpp
+++ test/loadertest2.cpp
@@ -3,6 +3,7 @@
 #include "../src/Layer.h"
 #include "../src/linear.h"
 #include "../src/relu.h"
+#include "../src/prediction.h"
 #include <iostream>
 #include <string>
 void printit(Tensor&x){
@@ -13,6 +14,7 @@ void printit(Tensor&x){
         if (i!=0&&i%x_col==0)std::cout << "\n";
         std::cout << X[i] << " ";
     }
+    std::cout << "\n";
 }
 void printit(float*x){
     for (int i=0;i<4;i++){
@@ -31,12 +33,16 @@ int main (){
     std::cout << "input is \n";
     printit(in);
     sequence seq;
-    std::string wpath="data_set/weights.txt";
-    std::string bpath="data_set/bias.txt";
-    seq.add(new Linear (4,2,wpath,bpath));
+    std::string wpath="data_set/weights1.txt";
+    std::string bpath="data_set/bias1.txt";
+    std::string wpath2="data_set/weights2.txt";
+    std::string bpath2="data_set/bias2.txt";
+    seq.add(new Linear (4,3,wpath,bpath));
     seq.add(new Relu);
+    seq.add(new Linear (3,10,wpath2,bpath2));
     std::cout << "output is \n";
     Tensor output=seq.forward(input);
     printit(output);
+    std::cout << "final ans : " << prediction(output) << "\n";
     return 0;
 }
