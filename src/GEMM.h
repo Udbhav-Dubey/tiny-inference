f52820e85f8c8dcf3d5904928c2bddd4b0e6b4c4
Udbhav-Dubey <udbhavdubey44@gmail.com>
2026-07-06 18:12:30 +0530

    alright benchmarked simd tiled

--- src/GEMM.h
+++ src/GEMM.h
@@ -4,4 +4,4 @@ Tensor Gemm(Tensor&,Tensor&);
 Tensor Gemm_ijk(Tensor&,Tensor&);
 Tensor Gemm_tiled(Tensor&,Tensor&,int block_size);
 Tensor Gemm_simd(Tensor&,Tensor&);
-Tensor Gemm_simd_tiled(Tensor&,Tensor&,int block);
+Tensor Gemm_tiled_simd(Tensor&,Tensor&,int block);
